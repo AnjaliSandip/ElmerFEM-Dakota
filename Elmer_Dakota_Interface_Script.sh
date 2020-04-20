ff32fc00f084506afb3edd361579141ac66e1e04
Ilajna2 <60862184+Ilajna2@users.noreply.github.com>
2020-04-20 18:34:28 -0500

    Update Elmer_Dakota_Interface_Script.sh

--- Elmer_Dakota_Interface_Script.sh
+++ Elmer_Dakota_Interface_Script.sh
@@ -13,6 +13,7 @@
 
 
 #!/bin/bash
+#This is a generic interface script for coupling Elmer with Dakota
 
 # $1 and $2 are special variables in bash that contain the 1st and 2nd 
 # command line arguments to the script, which are the names of the
