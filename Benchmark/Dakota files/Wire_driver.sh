aea7ff4da48d4f20e5eb2437659993e82096d409
Anjali Sandip <60862184+AnjaliSandip@users.noreply.github.com>
2020-08-05 18:52:44 -0500

    Add files via upload

---
+++ Benchmark/Dakota files/Wire_driver.sh
@@ -0,0 +1,69 @@
+ # copyright (c) 2019 ASandip
+ # This program is free software: you can redistribute it and/or 
+ # modify it under the terms of the GNU General Public License as 
+ # published by the Free Software Foundation, either version 3 of the License, or
+ # (at your option) any later version.
+ # This program is distributed in the hope that it will be useful,
+ # but WITHOUT ANY WARRANTY; without even the implied warranty of
+ # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ # GNU General Public License for more details.
+ 
+ # You should have received a copy of the GNU General Public License
+ # along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+#!/bin/bash
+
+# $1 and $2 are special variables in bash that contain the 1st and 2nd 
+# command line arguments to the script, which are the names of the
+# Dakota parameters and results files, respectively.
+
+params=$1
+results=$2
+
+############################################################################### 
+##
+## Pre-processing Phase: Transfer new set of values for the chosen input 
+## parameters from Dakota to Elmer Fem
+##
+###############################################################################
+#Chosen input parameter: wire radius
+dprepro $params wire.template wire.grd
+
+# Transfer Elmer geometry file, wire.grd, with the new wire radius from the Dakota to Elmer workspace
+cp /Dakota/Gui/Workspace/Dakota/Project/wire.grd   /Elmer/Workspace/Project/Folder/wire.grd
+
+#Chosen input parameter: electric conductivity of the wire
+dprepro --inline "@ @" $params case.template case.sif
+
+# Transfer Elmer simulation input file, case.sif, with the new electric conductivity from the Dakota workspace to the Elmer workspace
+cp /Dakota/Gui/Workspace/Dakota/Project/case.sif  /Elmer/Workspace/Project/Folder/case.sif
+
+
+###############################################################################
+## Execution Phase: Run your simulation
+##
+###############################################################################
+##Create new mesh (and the corresponding mesh files) for the new geometry, create wire folder
+cd /Elmer/Workspace/Project/Folder/
+ElmerGrid 1 2 wire.grd
+
+#Transfer the revised wire.grd files to the wire folder
+cp /Elmer/Workspace/Project/Folder/wire.grd    /Elmer/Workspace/Project/Folder/wire/wire.grd
+
+##Open and run Elmer project, wire, with revised geometry and mesh files
+cd   /Elmer/Workspace/Project/Folder/wire
+ElmerSolver case.sif
+
+############################################################################### 
+##
+## Post-processing Phase: Extract (or calculate) quantities of interest
+##  from your simulation's output and write them to a properly-formatted
+##  Dakota results file.
+##
+###############################################################################
+
+#Extract the joule heating at the final timestep from Elmer output and transfer it to Dakota results file, results.out
+cp  /Elmer/Workspace/Project/Folder/results.txt   $results
+
+
+
---
+++ Benchmark/README.md
@@ -0,0 +1,25 @@
+Electromagnetics (3D, Steady state)
+
+The interface program was validated against a 3D, steady state benchmark test: magnetic field induced by a potential difference applied over a wire of finite length [1, 2].  
+The material properties of the wire are that of copper.  The wire is aligned in the z direction and is surrounded by air.  
+The objective was to determine the effect of changes in wire radius (+ 10%) and electric conductivity (+ 2 x 106 A/m-V) on maximum field value for joule heating. 
+
+
+Analytical model was implemented in Matlab.  The numerical model was built in Elmer FEM using unstructured, hexahedral mesh elements and quadratic linear elements.  
+Elmer FEM’s electromagnetic solver, WhitneyAV, was chosen for this study.  The solver implements classic Lagrange interpolation and edge element basis functions to approximate the scalar 
+and the vector potentials, respectively [2]. 
+
+
+Latin hypercube sampling study was performed, with a sample size of 400.  The input parameters were distributed normally.  
+Asynchronous local parallelism was implemented, 4 evaluations performed concurrently.  The results from the numerical model were in good agreement with the analytical model as shown in Fig. 2.  
+The numerical model predicts that for the changes in the wire’s material and geometric parameters, there is a 95% probability that the maximum field value for joule heating would vary between 4 and 11 watts.  
+In addition, design and analysis of computer experiments (DACE) technique was employed to generate the training data, a sample size of 50 was chosen.  
+A linear regression fit was applied to the training data to construct the global surrogate model.  Latin hypercube sampling study was then performed on the global surrogate model. 
+Were the results of the sampling study on surrogate model same as the truth? Should I mention the reduction in the CPU time.  
+The DACE technique was shown to reduce the total run time by 87% for the benchmark test. 
+
+
+
+References:
+[1] Bossavit A. Computational electromagnetism: variational formulations, complementarity, edge elements. Academic Press; 1998.  
+[2] Råback P, Malinen M, Ruokolainen J, Pursula A, Zwinger T. Elmer models manual. Computation of magnetic fields in 3D, CSC–IT Center for Science, Helsinki, Finland. 2013 Feb 1.  
